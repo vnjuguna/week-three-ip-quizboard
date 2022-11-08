04bd653babc51164b2007f9a11604f4ba1d693a7
Violet <vnjuguna@gmail.com>
2022-11-08 22:04:47 +0300

    add styling and ammend business logic

--- js/quizboard.js
+++ js/quizboard.js
@@ -1,37 +1,103 @@
 const quizAnswers = document.querySelectorAll(".ouranswers");
 
+ 
+
 const submitAnswer = document.querySelector("#submit_ans");
+
+ 
+
+const mymarks = document.querySelector("#yourmarks")
 
 let marks = 0;
 
+ 
+
 let total = 0;
+
+ 
 
 quizAnswers.forEach(function (jibu) {
 
+ 
+
     if(parseInt(jibu.value) > 0) {
+
+ 
 
         total += parseInt(jibu.value);
 
     }
 
-    else{
-
-        total = total;
-
-    }
-
-    jibu.addEventListener("change", function (event) {
-
-        marks += parseInt(event.target.value);
-
-    });
-
 });
+
+ 
 
 submitAnswer.addEventListener("click", function (event) {
 
+    quizAnswers.forEach(function(jibu){
+
+        if (jibu.checked){
+
+            marks += parseInt(jibu.value);
+
+        }
+
+ 
+
+    })
+
+   
+
     let score = (marks/total)*100;
 
-    alert("your score is " + score + "%");
+    let message = "";
+
+ 
+
+    if (score >= 80){
+
+        message += "your score is " + score + "%. Excellent!"
+
+    }
+
+    else if (score >= 50 && score < 80){
+
+        message += "your score is " + score + "%. Fairly Passed!"
+
+    }
+
+    else{
+
+        message += "your score is " + score + "%. Poor result!"
+
+    }
+
+ 
+
+    mymarks.textContent = message;
+
+ 
+
+   
+
+ 
 
 });
+
+ 
+
+$("document").ready(function(){
+
+   
+
+ 
+
+    $("#submit_ans").click(function(){
+
+       
+
+        $("#display").removeClass("d-none");
+
+    })
+
+})
