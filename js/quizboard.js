316e8a9af9ebc7955e3cf950514da38315ed4a99
Violet <vnjuguna@gmail.com>
2022-11-08 22:16:32 +0300

    make edits on the html file

--- js/quizboard.js
+++ js/quizboard.js
@@ -1,103 +1,43 @@
 const quizAnswers = document.querySelectorAll(".ouranswers");
 
- 
-
 const submitAnswer = document.querySelector("#submit_ans");
 
- 
-
-const mymarks = document.querySelector("#yourmarks")
+const mymarks = document.querySelector("#yourmarks");
 
 let marks = 0;
 
- 
-
 let total = 0;
 
- 
-
 quizAnswers.forEach(function (jibu) {
-
- 
-
-    if(parseInt(jibu.value) > 0) {
-
- 
-
-        total += parseInt(jibu.value);
-
-    }
-
+  if (parseInt(jibu.value) > 0) {
+    total += parseInt(jibu.value);
+  }
 });
-
- 
 
 submitAnswer.addEventListener("click", function (event) {
-
-    quizAnswers.forEach(function(jibu){
-
-        if (jibu.checked){
-
-            marks += parseInt(jibu.value);
-
-        }
-
- 
-
-    })
-
-   
-
-    let score = (marks/total)*100;
-
-    let message = "";
-
- 
-
-    if (score >= 80){
-
-        message += "your score is " + score + "%. Excellent!"
-
+  quizAnswers.forEach(function (jibu) {
+    if (jibu.checked) {
+      marks += parseInt(jibu.value);
     }
+  });
 
-    else if (score >= 50 && score < 80){
+  let score = (marks / total) * 100;
 
-        message += "your score is " + score + "%. Fairly Passed!"
+  let message = "";
 
-    }
+  if (score >= 80) {
+    message += "your score is " + score + "%. Excellent!";
+  } else if (score >= 50 && score < 80) {
+    message += "your score is " + score + "%. Fairly Passed!";
+  } else {
+    message += "your score is " + score + "%. Poor result!";
+  }
 
-    else{
-
-        message += "your score is " + score + "%. Poor result!"
-
-    }
-
- 
-
-    mymarks.textContent = message;
-
- 
-
-   
-
- 
-
+  mymarks.textContent = message;
 });
 
- 
-
-$("document").ready(function(){
-
-   
-
- 
-
-    $("#submit_ans").click(function(){
-
-       
-
-        $("#display").removeClass("d-none");
-
-    })
-
-})
+$("document").ready(function () {
+  $("#submit_ans").click(function () {
+    $("#display").removeClass("d-none");
+  });
+});
